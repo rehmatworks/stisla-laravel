7634aae3becdb98bebbbb634da316a68ea18394d
Santi Aguilar <Santeee@users.noreply.github.com>
2020-05-10 01:15:32 -0300

    Update AdduserComponent.vue
    Fixing v-bind:value in v-for. v-bind:key added.

--- resources/js/components/AdduserComponent.vue
+++ resources/js/components/AdduserComponent.vue
@@ -31,7 +31,7 @@
                     <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Role</label>
                     <div class="col-sm-12 col-md-7">
                         <select class="form-control" v-model="role" v-bind:class="{'is-invalid': errors.role}">
-                            <option v-for="role in roles" v-bind:value="role.id">{{ role.name }}</option>
+                            <option v-for="role in roles" v-bind:key="role.id">{{ role.name }}</option>
                         </select>
                         <div class="invalid-feedback" v-if="errors.role">
                             <p>{{ errors.role[0] }}</p>
